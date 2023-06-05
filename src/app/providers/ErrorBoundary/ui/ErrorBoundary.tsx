ded1b77fc92682fb6d17d0fe1b4bfa9e60607700
XTimkakorXONE <korolevartem848@gmail.com>
2023-06-05 15:55:47 +0300

    change ErrorBoundary

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import React, { ErrorInfo, ReactNode, Suspense } from 'react';
-import { ErrorPage } from 'widgets/ErrorPage/ui/ErrorPage';
+import { PageError } from 'widgets/PageError';
 
 interface ErrorBoundaryProps {
     children: ReactNode;
@@ -34,7 +34,7 @@ class ErrorBoundary
             // You can render any custom fallback UI
             return (
                 <Suspense fallback="">
-                    <ErrorPage />
+                    <PageError />
                 </Suspense>
             );
         }
