c0d577cd3bb0ca45daeea9be4fe0eaff16470aec
XTimkakorXONE <korolevartem848@gmail.com>
2023-06-05 15:44:56 +0300

    init

---
+++ src/widgets/PageError/ui/PageError.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { PageError } from './PageError';
+
+export default {
+    title: 'widget/PageError',
+    component: PageError,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof PageError>;
+
+const Template: StoryFn<typeof PageError> = (args) => <PageError {...args} />;
+
+export const Light = Template.bind({});
+Light.args = {
+};
+Light.decorators = [ThemeDecorator(Theme.LIGHT)];
+
+export const Dark = Template.bind({});
+Dark.args = {};
