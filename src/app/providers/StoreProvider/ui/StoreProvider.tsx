b68768f4f0422936f49878f4764eaf122d9ae43f
XTimkakorXONE <korolevartem848@gmail.com>
2023-06-22 17:07:59 +0300

    add Reducer Manager for code spliting

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,17 +1,19 @@
 import { Provider } from 'react-redux';
 import { ReactNode } from 'react';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { createReduxStore } from '../config/store';
 import { StateSchema } from '../config/StateSchema';
 
 interface StoreProviderProps {
     children?: ReactNode;
     initialState?: StateSchema
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
-    const { children, initialState } = props;
+    const { children, initialState, asyncReducers } = props;
 
-    const store = createReduxStore(initialState);
+    const store = createReduxStore(initialState, asyncReducers as ReducersMapObject<StateSchema>);
 
     return (
         <Provider store={store}>
